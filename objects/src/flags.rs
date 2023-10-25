2d38a898ef10857a9074a64f8b46548889b57d3e
Papp Adam <papp.adam@students.jedlik.eu>
2023-10-25 20:48:45 +0200

    Szia helo

--- objects/src/flags.rs
+++ objects/src/flags.rs
@@ -1,136 +1,63 @@
 use std::fmt::Debug;
 
-use derive::Flag;
-
 pub trait Flag {
+    const SIZE: usize;
     fn into_usize(&self) -> usize;
 }
 
-#[derive(Flag, Clone, Copy)]
-pub enum GameObjectFlag {
-    None = 0,
-    NotClickable,
-    Test1,
-    Test2,
-    Test3,
+#[derive(Clone, Copy)]
+pub struct Flags<const SIZE: usize> {
+    _data: [u8; SIZE],
 }
 
-#[repr(C)]
-pub struct Flags<T: Flag> {
-    data: T,
-}
-
-impl<T: Flag> Flags<T> {
-    pub fn has_flag(&self, flag: T) -> bool {
-        unsafe {
-            GameObjectFlag;
-            let flag = flag.into_usize();
-            let mut _data_ptr = (&self.data as *const _ as usize + flag / 8 as usize) as *mut u8;
-            let element = flag % 8;
-            return *_data_ptr & (1 << element) == (1 << element);
-        }
-    }
-
-    pub fn activate_flag(&mut self, flag: T) {
-        unsafe {
-            let flag = flag.into_usize();
-            let mut _data_ptr = (&self.data as *const _ as usize + flag / 8 as usize) as *mut u8;
-            *_data_ptr = *_data_ptr | (1 << (flag % 8));
-        }
-    }
-
-    pub fn deactivate_flag(&mut self, flag: T) {
-        unsafe {
-            let flag = flag.into_usize();
-            let mut _data_ptr = (&self.data as *const _ as usize + flag / 8 as usize) as *mut u8;
-            *_data_ptr = *_data_ptr & !(1 << (flag % 8));
-        }
+impl<const SIZE: usize> Default for Flags<SIZE> {
+    fn default() -> Self {
+        let data = [0u8; SIZE];
+        Self { _data: data }
     }
 }
 
-impl<T: Flag> Debug for Flags<T> {
+impl<const SIZE: usize> Flags<SIZE> {
+    pub fn add_flag<T: Flag + Default + Copy>(&mut self, flag: T) {
+        let u_flag = flag.into_usize();
+        let p_chunk = &mut self._data[u_flag / 8] as *mut u8;
+        let bit = u_flag % 8;
+        unsafe {
+            *p_chunk = *p_chunk | (1u8 << bit as u8);
+        }
+    }
+
+    pub fn has_flag<T: Flag + Default + Copy>(&self, flag: T) -> bool {
+        let u_flag = flag.into_usize();
+        let p_chunk = &self._data[u_flag / 8] as *const u8;
+        let bit = u_flag % 8;
+        unsafe { *p_chunk & (1u8 << bit as u8) == (1u8 << bit as u8) }
+    }
+
+    pub fn remove_flag<T: Flag + Default + Copy>(&mut self, flag: T) {
+        let u_flag = flag.into_usize();
+        let p_chunk = &mut self._data[u_flag / 8] as *mut u8;
+        let bit = u_flag % 8;
+        unsafe {
+            *p_chunk = *p_chunk & !(1u8 << bit as u8);
+        }
+    }
+
+    pub fn toggle_flag<T: Flag + Default + Copy>(&mut self, flag: T) {
+        if self.has_flag(flag) {
+            self.remove_flag(flag);
+        } else {
+            self.add_flag(flag);
+        }
+    }
+}
+
+impl<const SIZE: usize> Debug for Flags<SIZE> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        unsafe { write!(f, "{:b}", *(&self.data as *const _ as *const u8)) }
-    }
-}
-
-// #[derive(Default, Debug, PartialEq, Clone, Copy)]
-// pub enum ObjectTag {
-//     #[default]
-//     Empty,
-//     Map,
-
-//     //Index in map's structure ObjectVector
-//     Structure(usize),
-// }
-
-// impl<'a> GameObject<'a> {
-//     ///Checks if an object has a certain tag.
-//     pub fn has_tag(&self, tag: &ObjectTag) -> bool {
-//         self.tags.contains(&tag)
-//     }
-//     ///Tries to add a tag to an object. If the tag is already present, no change will be made.
-//     ///Returns true if tag was added, false if nothing changed.
-//     pub fn add_tag(&mut self, tag: ObjectTag) -> bool {
-//         if !self.has_tag(&tag) {
-//             self.tags.push(tag);
-//             return true;
-//         }
-//         false
-//     }
-//     ///Forces a tag on an object, even if the tag is present.
-//     ///DO NOT USE UNLESS COMPLETELY NESCESSARY
-//     pub fn force_tag(&mut self, tag: ObjectTag) {
-//         self.tags.push(tag);
-//     }
-
-//     ///Tries to remove a tag from an object.
-//     ///Returns false if tag wasn't present, true if the tag was removed.
-//     ///Only first occurance is removed, unexpected behavior can happen if you've used force_tag() before.
-//     pub fn remove_tag(&mut self, tag: &ObjectTag) -> bool {
-//         for (i, object_tag) in self.tags.iter().enumerate() {
-//             if tag == object_tag {
-//                 self.tags.swap_remove(i);
-//                 return true;
-//             }
-//         }
-//         false
-//     }
-// }
-
-#[cfg(test)]
-mod tests {
-    use crate::flags::Flags;
-    use super::Flag;
-
-    #[derive(Default, Clone, Copy, Flag)]
-    enum TestFlag {
-        #[default]
-        Test0 = 0,
-        Test1 = 1,
-        Test2 = 2,
-        Test3 = 3,
-        Test4 = 4,
-        Test5 = 5,
-        Test6 = 6,
-        Test7 = 7,
-        Test8 = 8,
-        Test9 = 9,
-        Test10 = 10,
-        Test11 = 11,
-    }
-
-    #[test]
-    fn test() {
-        let flag_1 = TestFlag::Test0;
-        // let mut testflag = Flags {
-        //     data: TestFlag::default(),
-        // };
-        // testflag.activate_flag(TestFlag::Test1);
-        // testflag.activate_flag(TestFlag::Test11);
-        // assert!(testflag.has_flag(TestFlag::Test1));
-        // testflag.deactivate_flag(TestFlag::Test1);
-        // assert!(!testflag.has_flag(TestFlag::Test1));
-        // assert!(!testflag.has_flag(TestFlag::Test11));
+        let mut res = String::new();
+        for i in 0..SIZE * 8 {
+            res += &format!("{:b}", self._data[i]);
+        }
+        write!(f, "{res}")
     }
 }
